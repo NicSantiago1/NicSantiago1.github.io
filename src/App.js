8d299195e572f953178318e511e4333bdea4ac89
Nicolas Santiago <57651231+NicSantiago1@users.noreply.github.com>
2023-01-18 16:32:32 -0500

    Adding map data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
+import Map from './Map';
 
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibnNhbnRpYWdvMTgiLCJhIjoiY2xjcjQybXE1MGJ5ZDN2bWZtbXlzemRwYyJ9.MVzA2sRffOdZfca9fTiNtQ';
-
-export default function App() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(16.9568);
-    const [lat, setLat] = useState(48.0806);
-    const [zoom, setZoom] = useState(3);
-
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-          container: mapContainer.current,
-          style: 'mapbox://styles/mapbox/streets-v12',
-          center: [lng, lat],
-          zoom: zoom
-        });
-    });
-
-    useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-            map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-    });
-
+function App() {
     return (
         <div>
-            <div ref={mapContainer} className="map-container" />
-            <div className="map-overlay">
-                <form class="form">
-                    <input id="search" class="input" placeholder="Search..." />
-                </form>
-            </div>
+            <Map />
         </div>
-    );
+    )
 }
+
+export default App;
---
+++ src/Map.js
@@ -0,0 +1,71 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import countries from './geodata/countries.geojson';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibnNhbnRpYWdvMTgiLCJhIjoiY2xjcjQybXE1MGJ5ZDN2bWZtbXlzemRwYyJ9.MVzA2sRffOdZfca9fTiNtQ';
+
+const Map = () => {
+    const mapContainer = useRef(null);
+    const [map, setMap] = useState(null);
+
+    // Initialize the map
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [16, 48],
+          zoom: 3
+        });
+
+        map.on('load', () => {
+            map.addSource('countries', {
+                'type': 'geojson',
+                countries
+            });
+
+            map.setLayoutProperty('country-label', 'text-field', [
+                'format',
+                ['get', 'name_en'],
+                { 'font-scale': 1.2 },
+                '\n',
+                {},
+                ['get', 'name'],
+                {
+                  'font-scale': 0.8,
+                  'text-font': [
+                    'literal',
+                    ['DIN Offc Pro Italic', 'Arial Unicode MS Regular']
+                  ]
+                }
+              ]);
+    
+              map.addLayer({
+                  id: 'countries',
+                  type: 'fill',
+                  source: 'countries',
+                  'source-layer': 'countries'
+                },
+                'country-label'
+              );
+            
+              map.setPaintProperty('countries', 'fill-color', '#0000FF');
+    
+              setMap(map);
+        });
+
+        return () => map.remove();
+    }, []);
+
+    return (
+        <div>
+            <div ref={mapContainer} className="map-container" />
+            <div className="map-overlay">
+                <form class="form">
+                    <input id="search" class="input" placeholder="Search..." /> 
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Map;
