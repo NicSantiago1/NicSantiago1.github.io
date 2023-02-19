6edabf07b414fd63b7948f060ed5b2dddcf1f73f
NicSantiago1 <57651231+NicSantiago1@users.noreply.github.com>
2023-02-19 17:28:08 -0500

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const darkTheme = createTheme({
@@ -17,7 +16,7 @@ const darkTheme = createTheme({
       },
 });
 
-const filters = ['Year', 'Country'];
+const filters = ['Year', 'Country', 'Migration Flow'];
 
 export default function NavBar() {
     return(
@@ -25,13 +24,16 @@ export default function NavBar() {
             <ThemeProvider theme={darkTheme}>
                 <AppBar position="static" color="primary">
                     <Toolbar>
-                        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                            <MenuIcon />
-                        </IconButton>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             Migration Data Project
                         </Typography>
-                        
+                        <Box sx={{ flexGrow: 7, display: { xs: 'none', md: 'flex' } }}>
+                            {filters.map((filter) => (
+                            <Button key={filter} sx={{ my: 2, color: 'white', display: 'block' }}>
+                                {filter}
+                            </Button>
+                        ))}
+                        </Box>
                     </Toolbar>
                 </AppBar>
             </ThemeProvider>
