859e4fdd7f5bb0797778b9f6fb8bbc300e6dd79e
stevencabral21 <steven.cabral@ufl.edu>
2023-04-06 10:45:14 -0400

    Updates countries.geojson with migration data
    adds migration data from 2000-2021 for each country

--- src/geodata/netmigrationtuples.py
+++ src/geodata/netmigrationtuples.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import json
 
+with open('countries.geojson') as f:
+    geojson_data = json.load(f)
+
 data = pd.read_excel('NetMigrationData.xls', skiprows=3)
 
 years_data = {
-    str(year): [{"country": country, "migrants": net} for country, net in zip(data['Country Code'], data[str(year)]) if not pd.isna(net)]
+    str(year): {code: net for code, net in zip(data['Country Code'], data[str(year)]) if not pd.isna(net)}
     for year in range(2000, 2022) if str(year) in data
 }
 
-with open('net_migration.json', 'w') as f:
-    json.dump(years_data, f)
+for feature in geojson_data['features']:
+    country_code = feature['properties']['ISO_A3']
+    for year, net in years_data.items():
+        feature['properties'][year] = net.get(country_code)
+
+# Write updated GeoJSON to file
+with open('countries_with_migration.geojson', 'w') as f:
+    json.dump(geojson_data, f)
