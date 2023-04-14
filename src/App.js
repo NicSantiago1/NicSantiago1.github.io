ef211d22fd5dde62a53dd0aea44800534001abf9
mm <68615572+maximilian-mathers@users.noreply.github.com>
2023-04-14 12:12:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import Map from './Map';
 
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+  } from 'chart.js';
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+  );
+
 function App() {
     return (
         <div>
